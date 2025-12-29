7d81ca05f2f0843f1e3797c65b3f9000c466b9ef
sanpa <127140452+Sanpa33@users.noreply.github.com>
2025-12-29 20:02:26 -0300

    termina ui , y agrega funciones para adivinar palabra

--- src/App.jsx
+++ src/App.jsx
@@ -19,15 +19,26 @@ import { languages } from "./languages";
 function App() {
   const [currentWord, setCurrentWord] = useState("react");
 
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  console.log(guessedLetters);
+
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
   const letterElements = currentWord
     .split("")
     .map((letter, index) => <span key={index}>{letter.toUpperCase()}</span>);
 
-  const lettersElementsButtons = alphabet
-    .split("")
-    .map((letter) => <button key={letter}>{letter.toUpperCase()}</button>);
+  function addGuessedLetter(letter) {
+    setGuessedLetters((prevLetters) =>
+      prevLetters.includes(letter) ? prevLetters : [...prevLetters, letter]
+    );
+  }
+
+  const lettersElementsButtons = alphabet.split("").map((letter) => (
+    <button onClick={() => addGuessedLetter(letter)} key={letter}>
+      {letter.toUpperCase()}
+    </button>
+  ));
 
   return (
     <main>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,11 @@
 export default function Header() {
   return (
-    <>
-      <header>
-        <h1>Language Slayer</h1>
-        <p>
-          Guess the word in under 8 attempts to keep the programming world safe
-          from Assembly!
-        </p>
-      </header>
-    </>
+    <header>
+      <h1>Language Slayer</h1>
+      <p>
+        Guess the word in under 8 attempts to keep the programming world safe
+        from Assembly!
+      </p>
+    </header>
   );
 }
