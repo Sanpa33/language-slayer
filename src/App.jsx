577a90a3929526763422c425fe3ac3380f3636d8
Santino Mayan <127140452+Sanpa33@users.noreply.github.com>
2026-01-01 23:06:27 -0300

    agrega funcion rengerGameStatus

--- src/App.jsx
+++ src/App.jsx
@@ -65,24 +65,32 @@ function App() {
     lost: isGameLost,
   });
 
+  function renderGameStatus() {
+    if (!isGameOver) {
+      return null;
+    }
+
+    if (isGameWon) {
+      return (
+        <>
+          <h2>You win!</h2>
+          <p>Well done! 🎉</p>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <h2>Game over!</h2>
+          <p>You lose! Better start learning Assembly 😭</p>
+        </>
+      );
+    }
+  }
+
   return (
     <main>
       <Header />
-      <section className={gameStatusClass}>
-        {isGameOver ? (
-          isGameWon ? (
-            <>
-              <h2>You win!</h2>
-              <p>Well done! 🎉</p>
-            </>
-          ) : (
-            <>
-              <h2>Game over!</h2>
-              <p>You lose! Better start learning Assembly 😭</p>
-            </>
-          )
-        ) : null}
-      </section>
+      <section className={gameStatusClass}>{renderGameStatus()}</section>
       <section className="language-chips">
         <Languages languages={languages} wrongGuessCount={wrongGuessCount} />
       </section>
