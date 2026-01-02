e655354940cf6e32f572d36a7f32035595d2a234
Santino Mayan <127140452+Sanpa33@users.noreply.github.com>
2026-01-01 22:22:29 -0300

    agrega cartel de victoria o derrota

--- src/App.jsx
+++ src/App.jsx
@@ -60,12 +60,28 @@ function App() {
     );
   });
 
+  const gameStatusClass = clsx("game-status", {
+    won: isGameWon,
+    lost: isGameLost,
+  });
+
   return (
     <main>
       <Header />
-      <section className="game-status">
-        <h2>You win!</h2>
-        <p>Well done! 🎉</p>
+      <section className={gameStatusClass}>
+        {isGameOver ? (
+          isGameWon ? (
+            <>
+              <h2>You win!</h2>
+              <p>Well done! 🎉</p>
+            </>
+          ) : (
+            <>
+              <h2>Game over!</h2>
+              <p>You lose! Better start learning Assembly 😭</p>
+            </>
+          )
+        ) : null}
       </section>
       <section className="language-chips">
         <Languages languages={languages} wrongGuessCount={wrongGuessCount} />
